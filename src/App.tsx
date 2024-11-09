35172fc3608191ba990cc50f11e470ce8728f7cb
nnnikserrrr <nnnikserrrr@icloud.com>
2024-11-09 22:24:04 +0400

    add: gitkeep file to app dir ref: remove React fragment from App.tsx, remove text from index.html

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import { ThemeProvider } from '@mui/material';
 import { theme } from './shared/style/theme';
 
 function App() {
-   return (
-      <>
-         <ThemeProvider theme={theme}></ThemeProvider>
-      </>
-   );
+   return <ThemeProvider theme={theme}></ThemeProvider>;
 }
 
 export default App;

39f45a7b8c43059b0a61cb531242b7f977b62c73
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2025-03-31 09:15:58 +0000

    fix: remove unused selectors from layout

--- src/pages/Registration/ui/RegistrationPage.tsx
+++ src/pages/Registration/ui/RegistrationPage.tsx
@@ -2,40 +2,42 @@ import Box from '@mui/material/Box';
 
 export const RegistrationPage = () => {
    return (
-      <Box
+      // <Box
+      // component='div'
+      // sx={{
+      //    backgroundColor: "red",
+      //    height:"100%",
+      //    width: "100%"
+      // }}
+      // >
+         <Box
          component='div'
          sx={{
-      
+            marginTop: "%",
             position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%,-50%)',
-            height: '70%',
-            width: '80%',
+            height: '80%',
+            width: '90%',
             borderRadius: '20px',
-            backgroundColor: '#bfd2b5',
+            backgroundColor: '#bfd7b5',
          }}
       >
          This Box renders as an HTML section element.
          <Box
             component='div'
             sx={{
-               color: 'white',
+               alignItems: "center",
                backgroundColor: '#5f9b4a',
-               position: 'absolute',
-               marginLeft: 'auto',
-               marginRight: 'auto',
-               left: '0',
-               right: '0',
-               textAlign: 'center',
-               borderRadius: '10px',
-               height: '50px',
-               verticalAlign: 'middle',
-               lineHeight: '45px',
+               display: 'flex',
+               height:"50px",
+               width: "90%",
+               borderRadius: "15px"
             }}
          >
             РЕГИСТРАЦИЯ
          </Box>
       </Box>
+      // </Box>
+
+      
    );
-};
+}

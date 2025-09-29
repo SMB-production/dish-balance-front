f5cea9691e9da458ce6bf69df8b93baab3723965
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2025-09-29 10:40:40 +0500

    replace reload to navigate

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -6,13 +6,15 @@ import { LangSwitcher } from '../../../shared/LangSwitcher/ui/LangSwitcher.tsx';
 import { theme } from '../../../shared/style/theme';
 import Box from '@mui/material/Box';
 import { postLogoutRequest } from '../../../shared/api/logout.ts';
+import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
    const { t } = useTranslation('translation');
+   const navigate = useNavigate();
    const HandleLogOut = async () => {
       try {
          await postLogoutRequest();
-         window.location.reload();
+         navigate('/login');
       } catch (error) {
          console.log(error);
       }

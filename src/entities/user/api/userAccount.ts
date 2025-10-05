f3d698b4a045dfbc6e06f1dc474264c7902cc370
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2025-10-05 15:49:00 +0500

    fix: review fix

--- src/entities/user/api/userAccount.ts
+++ src/entities/user/api/userAccount.ts
@@ -1,5 +1,5 @@
 import { baseAPI } from '@shared/api/api.ts';
-import { UserInfo } from '@pages/UserAccountPage/ui/UserAcount.tsx';
+import { type UserInfo } from '@entities/user/types/userInfoInterface.ts';
 
 export const getUserInformation = async (): Promise<UserInfo> => {
    const response = await baseAPI.get('auth/profile');
